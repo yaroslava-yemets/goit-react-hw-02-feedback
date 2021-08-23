4daf15f95fea4c001b6a99ce0ea47a7395437af1
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-08-23 06:05:06 +0300

    Update FeedbackOptions.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -17,6 +17,7 @@ const FeedbackOptions = ({onLeaveFeedback, options}) => (
 
 FeedbackOptions.propTypes = {
     onLeaveFeedback: PropTypes.func.isRequired,
+    options: PropTypes.arrayOf(PropTypes.string)
 };
 
 export default FeedbackOptions;
